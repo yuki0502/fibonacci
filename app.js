f744c8e28c17b1ff84f7b89051a8e773930368d0
yuki0502 <ki_yu_0502@yahoo.co.jp>
2018-08-05 08:00:31 +0000

    トリボナッチ数列の実装

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+//トリボナッチ数列
+'use strict';
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 0);
+memo.set(2, 1);
+function trib(n) {
+    if(memo.has(n)) {
+        return memo.get(n);
+    }
+    const value = trib(n - 1) + trib(n - 2) + trib(n - 3);
+    memo.set(n, value);
+    return value;
+}
+const length = 40;
+for (let i = 0; i <= length; i++) {
+    console.log(trib(i));
+}
+
+
+
+
+/*
+//fibonacci数列(アルゴリズム改善後)
 'use strict';
 const memo = new Map();
 memo.set(0, 0);
@@ -16,7 +40,8 @@ for (let i = 0; i <= length; i++) {
 }
 
 
-/*
+
+//fibonacci数列(アルゴリズム改善前)
 'use strict';
 function fib(n) {
     if (n === 0) {
